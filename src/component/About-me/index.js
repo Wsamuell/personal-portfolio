388a67a00c8e33de2792a4795dacc928b14c49ba
samuel Odubamowo <wemsamuel@gmail.com>
2021-07-31 01:39:47 -0700

    completed about me page

--- src/component/About-me/index.js
+++ src/component/About-me/index.js
@@ -9,9 +9,9 @@ function About() {
 
         <div className="about">
             <Hero />
-            <div className='flex flex-col w-screen justify-center items-center'>
-                <div className='text-justify about-paragraph max-w-2xl bg-white shadow-lg rounded-lg my-20'>
-                    <div className='md:justify-end -mt-16'>
+            <div className='flex flex-wrap w-screen justify-center items-center'>
+                <div className='text-justify about-paragraph max-w-2xl bg-white shadow-2xl rounded-lg my-20'>
+                    <div className='md:justify-center -mt-16'>
                         <img className='w-20 h-20 object-cover rounded-full' src={require("../../asset/img/mini.png").default} alt='authors-img' />
                     </div>
                     <h1 className='intro'>Hello There! I'm Sam... Nice you finally meet you</h1>
@@ -25,80 +25,60 @@ function About() {
                         <p>I have a modern perspective on life & I love designing contemporary styled products</p>
                     </div>
                 </div>
-                <article className="flex justify-evenly">
-                    <div className="text-center space-y-2 sm:text-left">
-                        <div className="space-y-0.5">
-                            <p className="text-lg text-black font-semibold">
-                                Front-end
-                            </p>
+                <br />
+                <article className="flex flex-wrap space-x-4 tools">
+                    <div className="text-center  about-paragraph flex-1 bg-white shadow-2xl rounded-lg my-20">
+                        <div className='flex justify-center'>
+                            <img className='w-20 h-20 text-center ux-img object-center' src={require("../../asset/img/front-end.png").default} alt='ux-img' />
+                        </div>
+                        <br />
+                        <div className="">
+
+                            <p className="tool-title">Front-End</p>
                             <p className="">
+                                <br />
                                 I value the UX perspective of design because as well all know, that what brings in the customers, for this reason i strive daily to become better at my craft as a front end designer
                             </p>
-                            <p className="font-medium">
-                                My Tools
-                            </p>
+                            <br />
+                            <p className="tool-title-sm">My Tools</p>
+                            <br />
                             <ul>
-                                <li>
-                                    React.Js
-                                </li>
-                                <li>
-                                    Javascript
-                                </li>
-                                <li>
-                                    CSS
-                                </li>
-                                <li>
-                                    Bootstrap
-                                </li>
-                                <li>
-                                    Handlebars
-                                </li>
-                                <li>
-                                    TailwindCSS
-                                </li>
-                                <li>
-                                    Bulma
-                                </li>
+                                <li>React.Js</li>
+                                <li>Javascript</li>
+                                <li>CSS</li>
+                                <li>Bootstrap</li>
+                                <li>Handlebars</li>
+                                <li>TailwindCSS</li>
+                                <li>Bulma</li>
                             </ul>
                         </div>
                     </div>
-                    <div className="text-center space-y-2 sm:text-left">
-                        <div className="space-y-0.5">
-                            <p className="text-lg text-black font-semibold">
-                                Back-end
-                            </p>
-                            <p className="">
-                                I dont just thieve on the Front-end, we can take it out back too
-                            </p>
-                            <p className="font-medium">
-                                My Tools
-                            </p>
-                            <ul>
-                                <li>
-                                    Node.js
-                                </li>
-                                <li>
-                                    Express.js
-                                </li>
-                                <li>
-                                    NoSql
-                                </li>
-                                <li>
-                                    Sql
-                                </li>
-                                <li>
-                                    MongoDB
-                                </li>
-                                <li>
-                                    Heroku
-                                </li>
-                            </ul>
+                    <div className="text-center about-paragraph flex-1 bg-white shadow-2xl rounded-lg my-20">
+                        <div className='flex justify-center'>
+                            <img className='w-20 h-20 text-center ux-img' src={require("../../asset/img/back-end.png").default} alt='ux-img' />
                         </div>
+                        <br />
+                        <p className="tool-title">Back-End</p>
+                        <br />
+                        <p className="">I don't just thieve on the Front-end, we can take it out Back(end)
+                        </p>
+                        <br />
+                        <p className="tool-title-sm"> My Tools</p>
+                        <br />
+
+                        <ul>
+                            <li>Node.js </li>
+                            <li>Express.js</li>
+                            <li>NoSql</li>
+                            <li>Sql</li>
+                            <li>MongoDB</li>
+                            <li>Heroku</li>
+                        </ul>
                     </div>
 
                 </article>
-                <a href="https://docs.google.com/document/d/1oIL6i7CuC1ZKXQhqjkA-LfXXpfh7NwebFpkGk8bAbF4/edit?usp=sharing" target="_blank"> My Resume!</a>
             </div>
+
         </div>
 
     )
