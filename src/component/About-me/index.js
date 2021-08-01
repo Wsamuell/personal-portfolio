52fcd248a0629efff2f3912410cca509525ac220
samuel Odubamowo <wemsamuel@gmail.com>
2021-07-31 18:40:25 -0700

    completed contacts page

--- src/component/About-me/index.js
+++ src/component/About-me/index.js
@@ -10,19 +10,21 @@ function About() {
         <div className="about">
             <Hero />
             <div className='flex flex-wrap w-screen justify-center items-center'>
-                <div className='text-justify about-paragraph max-w-2xl bg-white shadow-2xl rounded-lg my-20'>
+                <div className='about-paragraph max-w-3xl bg-white shadow-xl rounded-lg my-20'>
                     <div className='md:justify-center -mt-16'>
                         <img className='w-20 h-20 object-cover rounded-full' src={require("../../asset/img/mini.png").default} alt='authors-img' />
+                        <h1 className='intro text-center'>Hello There! I'm Sam... Nice to finally meet you</h1>
                     </div>
-                    <h1 className='intro'>Hello There! I'm Sam... Nice to finally meet you</h1>
-                    <div className='about-text'>
-                        <p>Ambitious, courageous, dedicated, and Hardworking</p>
+                    <div className='about-text text-justify'>
+                        <p>I've been looking forward to this day, and I've got to say... it was worth the wait</p>
                         <br />
-                        <p>These are some of the words used to describe me over my last couple of experiences either as a student or a professional in the industry. I'm willing to put in the time and the effort no one else would all with a smile on my face</p>
+                        <p>I'm a Goal oriented person. I love working towards accomplishing a goal or multiple goals. I don't want to just show up and do something, i want to make a difference no matter how small it is. You just have to start from somewhere </p>
                         <br />
-                        <p>My prior experiences tells so much about me and my success in many different aspects of service but I don't like to let past experiences define me since life is about growing and developing new skills. Life is about learning and teaching yourself new ways to get to your derived goal</p>
+                        <p>I live to laugh. If there is one thing college has thought me, its the fact that it takes 43 muscles to smile. Putting a smile on your face puts a smile on someone elses. Pass it on its contagious. </p>
                         <br />
-                        <p>I have a modern perspective on life & I love designing contemporary styled products</p>
+                        <p>I am a very simple person with big dreams, I have had my share of mistakes but I'll never let that stop me. No matter how big or small my challenge  it i will see it to the end</p>
+                        <br />
+                        <p>I am a Developer. Full Stack applications are my passion and I'll never let that die out! </p>
                     </div>
                 </div>
                 <br />
@@ -35,10 +37,8 @@ function About() {
                         <div className="">
 
                             <p className="tool-title">Front-End</p>
-                            <p className="">
-                                <br />
-                                I value the UX perspective of design because as well all know, that what brings in the customers, for this reason i strive daily to become better at my craft as a front end designer
-                            </p>
+                            <br />
+                            <p className=""> value the UX perspective of design because as well all know, that what brings in the customers</p>
                             <br />
                             <p className="tool-title-sm">My Tools</p>
                             <br />
--- src/component/Connect/index.js
+++ src/component/Connect/index.js
@@ -1,27 +1,30 @@
 import React, { useState } from 'react'
+import '../Connect/style.css'
+
 
 function Connect() {
 
     return (
-        <section className='flex justify-evenly'>
-            <form id='contact-form'>
-                <p>Contact</p>
-                <p>I'm seeking out opportunities to help me grow as a developer and engineer with a company/agencies or individuals. I would love to bring my high energy and inspiring ideas to help problem solve any task needed while gaining experience to be the best at what i do</p>
-                <br />
-                <p>Im available in office or remotely and also available to relocate</p>
-                <p>For any kind of inquires, please feel free to reach out through the platform below </p>
-
-                <div>
-                    <a href="mailto:wemsamuel@gmail.com"> Wemsamuel@gmail.com</a>
+        <section className='flex flex-col contact w-screen justify-center items-center'>
+            <p className='contact-header'>Lets get Connected</p>
+            <div className='contact-form flex-wrap max-w-3xl'>
+                <div className='object-contain'>
+                    <img src={require("../../asset/img/contact-dogs.jpg").default} alt='picture of dogs' />
                 </div>
-                <div>
-                    <a href="https://www.linkedin.com/in/samuel-wemimo-567229207/" target="_blank"> Linkedin</a>
+                <p className='contact-text text-center'>Lets admire the dogs together! For any kind of inquires, please feel free to reach out through the platform Below </p>
+                <div className='flex flex-col contact-form shadow-2xl rounded-lg  text-center'>
+                    <div className='flex flex-row justify-evenly contact-icons'>
+                        <a href="mailto:wemsamuel@gmail.com"><img className='w-20 h-20 text-center ux-img object-center' src={require("../../asset/img/gmail.png").default} alt='gmail-icon' /></a>
+                        <a href="https://www.linkedin.com/in/samuel-wemimo-567229207/" target="_blank"><img className='w-20 h-20 text-center ux-img object-center' src={require("../../asset/img/linkedin.png").default} alt='linkin-icon' /></a>
+                        <a href="https://github.com/Wsamuell" target="_blank"> <img className='w-20 h-20 text-center ux-img object-center' src={require("../../asset/img/github.png").default} alt='github-icon' /></a>
+                    </div>
+                    <div className='contact-text'>
+                        <p>I'm seeking out opportunities to help me grow as a developer and engineer with a company/agencies or individuals that shares the same motive/principles as myself. I would love to bring my high energy and inspiring ideas to help problem solve any task needed while gaining experience to be the best at what i do</p>
+                        <br />
+                        <p>Im available in office or remotely and also willing to relocate</p>
+                    </div>
                 </div>
-                <div>
-                    <a href="https://github.com/Wsamuell" target="_blank"> Github</a>
-                </div>
-
-            </form>
+            </div>
         </section>
     )
 }
