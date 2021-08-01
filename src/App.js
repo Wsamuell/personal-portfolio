372c1c6488b7001a03dab85bd17f295b7aa1b9f1
samuel Odubamowo <wemsamuel@gmail.com>
2021-07-31 19:21:56 -0700

    completed footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Render from './component/Render'
+import Footer from './component/Footer'
 import './App.css';
 
 
@@ -9,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Render />
-
+      <Footer />
     </div>
   );
 }
---
+++ src/component/Footer/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../Footer/style.css'
+
+
+
+function Footer() {
+
+    return (
+        <footer class="footer bg-white relative pt-1 border-b-2 ">
+            <div class="mt-16 border-t-2 border-gray-300 flex flex-col items-center">
+                <div class="sm:w-2/3 text-center py-6">
+                    <p class=" font-bold mb-2">
+                        © 2021 Created and styled by <span>Samuel O.</span>
+                    </p>
+                    <div className='flex flex-row justify-evenly text-center'>
+                        <a href="mailto:wemsamuel@gmail.com"><img className='w-20 h-20 text-center ux-img object-center' src={require("../../asset/img/gmail.png").default} alt='gmail-icon' /></a>
+                        <a href="https://www.linkedin.com/in/samuel-wemimo-567229207/" target="_blank"><img className='w-20 h-20 text-center ux-img object-center' src={require("../../asset/img/linkedin.png").default} alt='linkin-icon' /></a>
+                        <a href="https://github.com/Wsamuell" target="_blank"> <img className='w-20 h-20 text-center ux-img object-center' src={require("../../asset/img/github.png").default} alt='github-icon' /></a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+    )
+}
+
+
+
+
+
+
+
+export default Footer
