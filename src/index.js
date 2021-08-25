a3a2ce91d6e7a56ca42e64d08714d77f057b784e
samuel Odubamowo <wemsamuel@gmail.com>
2021-08-25 03:00:18 -0400

    strictmode

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import  { HashRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <HashRouter><App /></HashRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
