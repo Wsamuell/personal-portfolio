937749d6dd8942fc56d41693b9da49c842a590c0
samuel Odubamowo <wemsamuel@gmail.com>
2021-08-25 02:50:50 -0400

    hashrouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import  { HashRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <HashRouter><App /></HashRouter>,
   document.getElementById('root')
 );
 
