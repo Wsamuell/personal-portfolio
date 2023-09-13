af541e2e3a98080d88d8709a636f0cfb60cc4b92
Samuel Wemimo <wemsamuel@gmail.com>
2023-09-13 10:52:26 -0700

    new: style init and typescript init

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './App.css';
+
+type Props = {};
+
+const Header = (props: Props) => {
+  const styles = {};
+
+  return <div>Header</div>;
+};
+
+export default Header;
